bbd15e4378dc1c9b986799a141e6aad3adfb84ba
Mirafgan Eminbayli <mirefqan1999@gmail.com>
2023-08-20 22:29:05 +0400

    some rename fix

--- src/components/molecules/HamburgerMenu.tsx
+++ src/components/molecules/HamburgerMenu.tsx
@@ -9,13 +9,13 @@ function HamburgerMenu() {
 
   return (
     <div
-      className={`block w-full sm:w-max  min-h-[350px] lg:hidden fixed   ${activeClass} transition-all duration-300 top-[85px] bg-neutral-80 px-[24px]  py-[24px]`}
+      className={`block w-full sm:w-max  min-h-[350px] lg:hidden fixed ${activeClass} transition-all duration-300 top-[85px] bg-neutral-80 px-[24px]  py-[24px]`}
     >
       <div
         id='hamburger-wallet'
-        className='flex  items-start gap-[16px] p-[24px] border border-neutral-60'
+        className='flex items-start gap-[16px] p-[24px] border border-neutral-60'
       >
-        <img src={coin} alt='' />
+        <img src={coin} alt='coin' />
         <div>
           <span className='text-[14px] text-neutral-20 pb-[6px]'>Your Balance</span>
           <h3 className='font-monument text-[20px]'>7.00434 ETH</h3>
