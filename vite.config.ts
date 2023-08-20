0a29a49c5cfb4adaa09bf316eb4e66ba0123988d
Elcan Ali <130112208+Elcan-Ali@users.noreply.github.com>
2023-08-21 01:31:16 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/The-Largest-NFT-Marketplace-Netfly/",
+  base: "./The-Largest-NFT-Marketplace-Netfly",
 });
